8ac9a10a2f115e04c574bec2fe70edddbb83a4c8
UEEx <riyuueex@gmail.com>
2022-09-25 04:14:30 +0400

    With new background in the canvas section

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ $("#pick").click(function() {
   // Get a random name, the winner
   var winner = nameArray[Math.floor(Math.random()*nameArray.length)];
   
-  winner = "🎉" + " " + winner + " " + "🎉";
+  //winner = "🎉" + " " + winner + " " + "🎉";
 
 
   //looping through participants 
